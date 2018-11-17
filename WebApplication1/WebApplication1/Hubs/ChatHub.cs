67d82054949e6a87ae058df89cfafbdf9487f4f3
Homero Batista Kzam <homero@parkcode.mobi>
2018-11-17 10:32:01 -0200

    Init

---
+++ WebApplication1/WebApplication1/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication1.Hubs
+{
+  public class ChatHub : Hub
+  {
+    public async Task SendMessage(string user, string message)
+    {
+      await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+
+    public async Task Typing(string user)
+    {
+      await Clients.All.SendAsync("TypingMessage", user);
+    }
+  }
+}
